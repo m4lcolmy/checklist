ea7dd976988102c9c7f7a402eaefdfea8ac23cc6
m4lcolmy <hsnhsyntrkmn@gmail.com>
2026-08-17 11:22:30 +0300

    first commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE_VERSION = "v1";
+var CACHE_NAME = "ucus-oncesi-checklist-" + CACHE_VERSION;
+var ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    }).then(function(){ return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function(event){
+  if(event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      if(cached) return cached;
+      return fetch(event.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copy); });
+        return res;
+      }).catch(function(){ return cached; });
+    })
+  );
+});
